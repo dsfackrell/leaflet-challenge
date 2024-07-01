6779dd51cbab35b7c47ba07692ba79a29baf3811
dsfackrell <84728935+dsfackrell@users.noreply.github.com>
2024-07-01 17:11:15 -0500

    popup text

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,51 +1,88 @@
 // API endpoint
-let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
 //Get request to the URL
 d3.json(queryUrl).then(function(data){
-    createFeatures(data.features)
+
+    let featureList = data.features
+    let myMap = createMap(featureList)    
 });
 
-function createFeatures(earthquakeData) {
-    // create each earthquake point
-    function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-    }
+//create the markers
+function createMarkers(feature) {
+    
 
-    //Create the GeoJSON layer that contains the features and run onEachFeature once for each piece of data
-    let earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature : onEachFeature
-    });
+    let long = feature.geometry.coordinates[0]
+    let lat = feature.geometry.coordinates[1]
+    let depth = feature.geometry.coordinates[2]
+    let mag = feature.properties.mag
 
-    // Create the map
-    createMap(earthquakes);
+    let popUpText = `<h2>Location: ${feature.properties.place}</h2><ul><li> Magnitue: ${mag}</li><li>Depth: ${depth}</li></ul>`
+
+    let markerLocation = [lat, long]
+
+    let marker = L.circle(markerLocation, {
+        fillOpacity: 0.75,
+        color: "darkgreen",
+        fillColor: getColor(depth),
+        radius: mag * 5000
+    }).bindPopup(popUpText);
+
+    return marker
 }
 
-function createMap(earthquakes) {
+function getColor(depth) {
+    let color = ""
+
+    if(depth >= 90) {
+        color = "red"
+    } else if(depth < 90 && depth >= 70) {
+        color = "orangered"
+    } else if(depth < 70 && depth >= 50) {
+        color = "orange"
+    } else if(depth < 50 && depth >= 30) {
+        color = "yellow"
+    } else if (depth < 30 && depth >= 10) {
+        color = "greenyellow"
+    } else {
+        color = "green"
+    }
+
+    return color;
+}
+
+function createMap(data) {
+    let features = data;
+
+    let featureMarkers = []
+
+    features.forEach(element => {
+        let marker = createMarkers(element);
+
+        featureMarkers.push(marker)
+    });
+
+    let markerLayerGroup = L.layerGroup(featureMarkers)
+
+    let streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	})
+
+    let baseMaps = {
+        "Street Map": streetMap
+    }
+
+    let overlayMaps = {
+        "Earthquake Locations": markerLayerGroup
+    }
     
-  // Create the base layers.
-  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
+    let myMap = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 5,
+        layers: [streetMap]
+    });
 
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
+    L.control.layers(baseMaps, overlayMaps, {collapsed:false}).addTo(myMap)
 
-  let baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
-  };
-
-  let overlayMaps = {
-    Earthquakes: earthquakes
-  };
-
-  let myMap = L.map("map", {
-    center: [37.09, -95.71], zoom: 5, layers: [street, earthquakes]
-  });
-
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+    return myMap
 }
